50f22ee83a41d902e6a6b4a33c1bada63214fc1d
syamim ionic <syamim727@gmail.com>
2019-11-20 08:48:56 +0800

    finish all lecturer room function

--- src/app/lect-room/lect-room.page.ts
+++ src/app/lect-room/lect-room.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http'
+import { Http } from '@angular/http';
 import * as papa from 'papaparse';
 
 @Component({
@@ -9,15 +9,16 @@ import * as papa from 'papaparse';
 })
 export class LectRoomPage implements OnInit {
 
-	csvData: any[] = [];
-  row: any;
+	public csvData: any[] = [];
+  
+  public lecturer: Array<{ name: string; course: string ; room: string }> = [];
+  public allLecturer: Array<{ name: string; course: string; room: string }> = [];
 
-  constructor(private http: Http) { 
-  	this.readCsvData();
+  constructor(private http: Http) {
   }
 
   private readCsvData() {
-    this.http.get('assets/sample.csv')
+    this.http.get('assets/lecturer_room.csv')
       .subscribe(
       data => this.extractData(data),
       err => this.handleError(err)
@@ -27,9 +28,27 @@ export class LectRoomPage implements OnInit {
   private extractData(res) {
     let csvData = res['_body'] || '';
     let parsedData = papa.parse(csvData,{header: false}).data;
-    this.row = parsedData.length
     this.csvData = parsedData;
+  	for (let i = 0; i < this.csvData.length; i++) {
+      this.lecturer.push({
+        name: this.csvData[i][0].charAt(0).toUpperCase() + this.csvData[i][0].slice(1),
+        course: this.csvData[i][1],
+        room: this.csvData[i][2]
+      });
+    }
+    this.allLecturer = this.lecturer;
   }
+
+	onSearchTerm(ev: CustomEvent) {
+    this.lecturer = this.allLecturer;
+    const val = ev.detail.value;
+
+	  if ( val.trim() !== '') {
+	    this.lecturer = this.lecturer.filter(term => {
+	      return term.name.toLowerCase().indexOf(val.trim().toLowerCase()) > -1;
+	    });
+	  }
+	}
 
   private handleError(err) {
     console.log('something went wrong: ', err);
@@ -40,7 +59,9 @@ export class LectRoomPage implements OnInit {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 	}
+
   ngOnInit() {
+  	this.readCsvData();
   }
 
 }
