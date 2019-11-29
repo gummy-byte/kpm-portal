f4b3949ffde98557fd541f7475ce756fc0bad16d
syamim <syamim@ifcon.tech>
2019-11-29 09:17:25 +0800

    Problem with how to exit the app in user-switch.page.ts

--- src/app/user-switch/user-switch.page.ts
+++ src/app/user-switch/user-switch.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, Platform, AlertController, Events } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
@@ -8,12 +8,43 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['./user-switch.page.scss'],
 })
 export class UserSwitchPage implements OnInit {
-  constructor(
-  		public navCtrl: NavController
-  	) { }
+	public subscribe:any;
 
-  exitApp(){
-  	navigator['app'].exitApp();
+  constructor(
+  		public navCtrl: NavController,
+  		public platform: Platform,
+  		public alertCtrl: AlertController,
+  		public event: Events
+  	) 
+  {}
+	
+	ionWillEnter(){
+		this.subscribe = this.platform.backButton.subscribe(()=>{ navigator['app'].exitApp(); });
+	}
+
+  async presentAlertConfirm() {
+    const alert = await this.alertCtrl.create({
+      header: 'Confirmation',
+      message: 'Do you really want to exit this app',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Cancel');
+          }
+        }, {
+          text: 'Confirm',
+          handler: () => {
+						navigator['app'].exitApp();
+            console.log('Confirm');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
   
   ngOnInit() {
